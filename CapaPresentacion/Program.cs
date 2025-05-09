f68583c3d2de9a7b8f6b214d00086cdb60e8ad27
Don-Oscar <george.rc2204@gmail.com>
2025-05-08 21:16:34 -0500

    avance login

---
+++ CapaPresentacion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicacion
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}
+
+
+
+    
